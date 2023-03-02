b12b644298faf742bf136737359ca3c1cf389040
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-03-02 17:27:58 +0100

    risoluzione usando per la maggiore funzioni

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Funzioni
+
 //Funzione che crea un elemento con un evento click al suo interno
 function creaElemento(tagElemento, nomeClasse, difficolta, i){
     const elemento = document.createElement(tagElemento);
@@ -16,11 +17,51 @@ function creaElemento(tagElemento, nomeClasse, difficolta, i){
     )
     return elemento;
 }
+
 // Funzione che appende elementi al DOM
 function appendiElemento(elementoCreato, appendino){
     appendino.append(elementoCreato);
 }
 
+// Funzione che controlla la difficolta scelta e restituisce una classe specifica
+function selezioneDifficolta(difficolta){
+    let classeDifficolta = '';
+    switch(difficolta){
+        case '81':
+            classeDifficolta = 'cella-9';
+            break;
+        case '49':
+            classeDifficolta = 'cella-7';
+            break;
+        default: 
+            classeDifficolta = 'cella-10';
+    };
+    return classeDifficolta;
+}
+
+// Funzione che controlla la difficolta scelta e restituisce un numero di celle massime
+function numeroCelle(difficolta){
+    let maxCelle = 0;
+    switch(difficolta){
+        case '81': 
+            maxCelle = 81;
+            break;
+        case '49':
+            maxCelle = 49;
+            break;
+        default:
+            maxCelle = 100;
+    }; 
+    return maxCelle;
+}
+
+// Funzione che tramite un ciclo crea una tipologia e numero di elementi in base a dei criteri
+function cicloCrea(classeDifficolta,maxCelle,appendino){
+    for(let i = 1; i <= maxCelle ; i++){
+        const elemento = creaElemento('div','cella', classeDifficolta,i);
+        appendiElemento(elemento,appendino);
+    }
+}
 // --------
 
 // Main
@@ -36,27 +77,13 @@ gioca.addEventListener(
         tavoloGioco.innerHTML= '';
         // Presa del valore della difficolta 
         const difficolta = document.getElementById('difficolta').value;
-        let classeDifficolta = '';
-        let maxCelle = 0;
-        // Switch con cambio classe e massimo numero di celle in base alla difficolta scelta
-        switch(difficolta){
-            case '81':
-                classeDifficolta = 'cella-9';
-                maxCelle = 81;
-                break;
-            case '49':
-                classeDifficolta = 'cella-7';
-                maxCelle = 49;
-                break;
-            default: 
-                classeDifficolta = 'cella-10';
-                maxCelle = 100;
-        };
-        // Ciclo che crea un numero di celle in base alle scelte precedenti
-        for(let i = 1; i <= maxCelle; i++){
-            const elemento = creaElemento('div', 'cella',classeDifficolta , i);
-            appendiElemento(elemento,tavoloGioco);
-        }
+
+        // Controllo della difficolta scelta e assegnazione in delle variabili 
+        const classeDifficolta = selezioneDifficolta(difficolta);
+        const maxCelle = numeroCelle(difficolta);
+
+        // chimata alla funzione per il ciclo che crea le celle
+        cicloCrea(classeDifficolta,maxCelle,tavoloGioco);
     }
 )
 
